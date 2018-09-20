72ff0353d9a99c71f06221ae324662744245fd32
Jeff L <jeffrey.y.leong@gmail.com>
2018-09-20 00:09:54 -0400

    GAuth working + error handling + case of already signed in/not yet, List of rooms working, Sample query

---
+++ app/src/main/java/com/example/jeff/ec463swmp1/RoomClasses/Room1.java
@@ -0,0 +1,13 @@
+package com.example.jeff.ec463swmp1.RoomClasses;
+
+import android.content.Intent;
+
+public class Room1 {
+    public static final String RoomNum = "room number";
+
+    Intent intent = new Intent(this, DataSelectActivity.class);
+    intent.putExtra(RoomNum, 0);
+
+    startActivity(intent);
+
+}
---
+++ app/src/main/java/com/example/jeff/ec463swmp1/RoomSelectionActivity.java
@@ -0,0 +1,81 @@
+package com.example.jeff.ec463swmp1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.example.jeff.ec463swmp1.RoomClasses.*;
+
+public class RoomSelectionActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private static final Class[] CLASSES = new Class[]{
+            Room0.class,
+            Room1.class,
+            Room2.class,
+            Room3.class,
+            Room4.class,
+            Room5.class,
+            Room6.class,
+            Room7.class,
+            Room8.class,
+            Room9.class,
+            Room10.class,
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+        setContentView(R.layout.room_selection_activity);
+
+        // Set up ListView and Adapter
+        ListView listView = findViewById(R.id.list_view);
+
+        MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_2, CLASSES);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Class clicked = CLASSES[position];
+        startActivity(new Intent(this, clicked));
+    }
+
+    public static class MyArrayAdapter extends ArrayAdapter<Class> {
+
+        private Context mContext;
+        private Class[] mClasses;
+
+        public MyArrayAdapter(Context context, int resource, Class[] objects) {
+            super(context, resource, objects);
+
+            mContext = context;
+            mClasses = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(android.R.layout.simple_list_item_2, null);
+            }
+
+            ((TextView) view.findViewById(android.R.id.text1)).setText(mClasses[position].getSimpleName());
+
+            return view;
+        }
+    }
+}
