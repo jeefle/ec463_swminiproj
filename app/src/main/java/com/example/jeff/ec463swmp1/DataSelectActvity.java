c39d23759729bdc2acadacfba6a2b50c1ec7f326
Jeff L <jeffrey.y.leong@gmail.com>
2018-09-20 16:51:05 -0400

    fixed login issue

--- app/src/main/java/com/example/jeff/ec463swmp1/DataSelectActvity.java
+++ app/src/main/java/com/example/jeff/ec463swmp1/DataSelectActvity.java
@@ -13,9 +13,7 @@ public class DataSelectActvity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_select_actvity);
-
     }
-
 
     public void showTemp(View view) {
         Intent intent = new Intent(this, PlotTempActivity.class);
